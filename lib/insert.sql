6950ec0736b586cd695e36fb7068356d63788923
Seo Choi <seochoi56@gmail.com>
2022-10-19 23:50:47 -0400

    commit tree 1c3754f138a9077e958c3e7121ea0fbb395f33c4

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES 
+("Mr. Chocolate", 5, "M", "brown", "happy", true),
+("Rowdy", 13, "M", "black", "sad", true),
+("Tabitha", 7, "F", "black", "excited", true),
+("Sergeant Brown", 3, "M", "orange", "happy", true),
+("Melissa", 7, "F", "yellow", "happy", true),
+("Grinch", 9, "M", "grey", "happy", true),
+("Wendy", 12, "F", "calico", "happy", true),
+(NULL, 5, "M", "brown", "happy", true)
